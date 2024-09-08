43650075e97a49b4e885b34916da9398dc31421d
Joon <joon.suh2003@gmail.com>
2024-09-08 21:47:43 +1000

    Changed command "component" to "add"

--- src/ReadCircuitFile.cpp
+++ src/ReadCircuitFile.cpp
@@ -2,7 +2,7 @@
 // Additional components are currently limited to 10 inputs max and 1 output.
 //
 // Current supported functions are:
-// component: 	Adds a new component to the simulation
+// add: 		Adds a new component to the simulation
 // connect: 	Connects two components output and input given component names and index
 // drive: 		Manually sets the value of a component input
 // query:		Reads and prints the output of a requested component
@@ -15,36 +15,28 @@
 #include <Component.hpp>
 #include <Circuit.hpp>
 
+// Forwards definition of helper functions
+
 // Helper function for converting a string to ELogicLevel
-ELogicLevel hash_logic_level(std::string const& p_string) {
-    if (p_string == "LOW") return LOGIC_LOW;
-    else if (p_string == "HIGH") return LOGIC_HIGH;
-    else return LOGIC_UNDEFINED;
-}
+ELogicLevel hash_logic_level(std::string const& p_string);
 
 // Helper function for converting a string to EComponentType
-EComponentType hash_gate_type(std::string const& p_string) {
-    if (p_string == "wire") return WIRE;
-    else if (p_string == "not") return NOT_GATE;
-    else if (p_string == "and") return AND_GATE;
-    else if (p_string == "or") return OR_GATE;
-    else if (p_string == "xor") return XOR_GATE;
-    else return TYPE_UNDEFINED;
-}
+EComponentType hash_gate_type(std::string const& p_string);
 
+// Main loop
 int main() {
 	C_Circuit circuit = C_Circuit();
 	while (true) {
 		std::string Request;
 		std::cin >> Request;  // get the next word from the input stream
-		std::cout << "Processing input token: " << Request << std::endl;
+		//std::cout << "Processing input token: " << Request << std::endl;
 
 		if (Request[0] == '#') {
 			// a comment line
 			// get the rest of the line and ignore it
 			std::string DummyVar;
 			getline( std::cin, DummyVar );
-		} else if (Request.compare( "component" ) == 0 ) {
+		} else if (Request.compare( "add" ) == 0 ) {
 			std::string GateType;
 			std::string GateName;
 			std::cin >> GateType;
@@ -124,4 +116,22 @@ int main() {
 	}
   
   return 0;
+}
+
+
+// Helper function implementations
+
+ELogicLevel hash_logic_level(std::string const& p_string) {
+    if (p_string == "LOW") return LOGIC_LOW;
+    else if (p_string == "HIGH") return LOGIC_HIGH;
+    else return LOGIC_UNDEFINED;
+}
+
+EComponentType hash_gate_type(std::string const& p_string) {
+    if (p_string == "wire") return WIRE;
+    else if (p_string == "not") return NOT_GATE;
+    else if (p_string == "and") return AND_GATE;
+    else if (p_string == "or") return OR_GATE;
+    else if (p_string == "xor") return XOR_GATE;
+    else return TYPE_UNDEFINED;
 }
